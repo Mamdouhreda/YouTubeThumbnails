1a73ee925e446f99a1edd8a984752c184c95f512
mamdohreda <mamdohreda@gmail.com>
2025-03-30 15:41:12 +0200

    remove privacy from site map

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -10,12 +10,6 @@ export default function sitemap() {
       changeFrequency: "daily",
       priority: 1,
     },
-    {
-      url: `${baseUrl}/privacy`,
-      lastModified: currentDate,
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
     // Add other important URLs from your site here
   ];
 
